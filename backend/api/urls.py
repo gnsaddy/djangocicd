3d185683e261b4515255785f7758ff91db3c975e
Aditya Raj <aditya.x510@gmail.com>
2021-07-16 20:52:25 +0530

    update

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,9 +3,5 @@ from . import views
 
 urlpatterns = [
     path('user/', include('api.user_auth.urls')),
-    path('course/', include('api.course.urls')),
-    path('dashboard/', include('api.dashboard.urls')),
-    path('demo/', include('api.demo.urls')),
-    path('testimonial/', include('api.testimonial.urls')),
     path('', views.Home, name="api_home"),
 ]
