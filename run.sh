35a9214a6132b437cef2ad005b8609255d3480b0
Aditya Raj <aditya.x510@gmail.com>
2021-07-16 17:33:18 +0530

    update

--- run.sh
+++ run.sh
@@ -7,4 +7,8 @@ source ~/env/bin/activate
 pwd
 pip3 freeze
 ls
-pip3 install -r requirements.txt
+cd /home/ubuntu/djangocicd
+echo "----------------------------------------"
+pwd
+echo "----------------------------------------"
+sudo pip3 install -r requirements.txt
