feeb99d17d2a2ed6a1a1b36b8223fc2c450b2460
Aditya Raj <aditya.x510@gmail.com>
2021-07-16 17:13:22 +0530

    update

--- setup.sh
+++ setup.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-echo "testing"
-# sudo amazon-linux-extras install docker -y
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# chmod +x /usr/local/bin/docker-compose
-# sudo service docker start
-# sudo usermod -a -G docker ec2-user
-# sudo chmod 666 /var/run/docker.sock
-# sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-# sudo service docker start
-
-# source env/bin/activate
-
-# cd ~/djangocicd
-
-# pip3 install -r requirements.txt
-
-# python3 manange.py makemigrations
-
-# python3 manage.py migrate
-
+echo "Starting env"
+source ~/env/bin/activate
+echo "Env activated"
+source ~/env/bin/activate
+pwd
+pip3 freeze
+pip3 install -r requirements.txt
