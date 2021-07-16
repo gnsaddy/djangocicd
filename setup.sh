e0430288690a18159984a4aa784c2a61b42c93cf
Aditya Raj <aditya.x510@gmail.com>
2021-07-16 20:44:47 +0530

    update

--- setup.sh
+++ setup.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-
 echo "Starting env"
 source ~/env/bin/activate
 echo "Env activated"
 source ~/env/bin/activate
+pwd
+pip3 freeze
+ls
+cd /home/ubuntu/djangocicd
 
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-sudo amazon-linux-extras install docker -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt update -y
+
+sudo apt install docker-ce -y
+
+sudo usermod -aG docker ${USER}
+su - ${USER}
+sudo usermod -aG docker username
 sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo chmod 666 /var/run/docker.sock
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo service docker start
-
